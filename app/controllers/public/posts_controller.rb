efcf7600e45de6887ac49dc43c71cf983e2d9ecd
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2023-03-20 14:12:12 +0000

    [post_add]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -17,6 +17,13 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
   end
   
   private
