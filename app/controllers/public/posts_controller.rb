ba3ed605c3b65e48be01740de2db75fcda427c23
EC2 Default User <ec2-user@ip-172-31-8-82.ap-northeast-1.compute.internal>
2023-05-20 13:14:16 +0000

    [css]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -42,7 +42,7 @@ class Public::PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to post_path(@post.id)
+    redirect_to post_path(post.id)
   end
   
   def destroy
